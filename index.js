5363d80e3d210b58eed892915420c641f0a8a1ac
John Pau Guzman <guzmanjps@gmail.com>
2024-11-18 14:57:13 +0800

    experiments

--- index.js
+++ index.js
@@ -19,10 +19,17 @@ async function getLockfileData() {
     const contents = await fs.promises.readFile(lockfilePath, 'utf8');
     let d = {};
     [d.name, d.pid, d.port, d.password, d.protocol] = contents.split(':');
+    console.log("LOCKFILE DATA:", d);
     return d;
 }
 
 async function getSession(port, password) {
+    console.log("PARAMS:", {
+        headers: {
+            'Authorization': 'Basic ' + Buffer.from(`riot:${password}`).toString('base64')
+        },
+        agent: localAgent
+    });
     return (await fetch(`https://127.0.0.1:${port}/chat/v1/session`, {
         headers: {
             'Authorization': 'Basic ' + Buffer.from(`riot:${password}`).toString('base64')
@@ -100,7 +107,7 @@ async function waitForLockfile()
     do
     {
         helpData = await getHelp(lockData.port, lockData.password);
-        if(!helpData.events.hasOwnProperty('OnJsonApiEvent_chat_v4_presences')) {
+        if(!helpData.events.hasOwnProperty('OnJsonApiEvent')) {
             console.log('Retrying help data events...');
             helpData = null;
             await asyncTimeout(1500);
@@ -108,6 +115,7 @@ async function waitForLockfile()
     } while(helpData === null);
 
     console.log('Got PUUID...');
+    console.log("HELP DATA EVENTS:", helpData.events);
 
     try {
         await fs.promises.mkdir('./logs');
@@ -124,12 +132,32 @@ async function waitForLockfile()
     const ws = new WebSocket(`wss://riot:${lockData.password}@127.0.0.1:${lockData.port}`, {
         rejectUnauthorized: false
     });
+    console.log("WEBSOCKET URL:", `wss://riot:${lockData.password}@127.0.0.1:${lockData.port}`)
+
+    localHelpEvents = [
+        "AgentResourceEvent",
+        "OnClientFlash",
+        "OnClientFocus",
+        "OnClientMinimize",
+        "OnJsonApiEvent",
+        "OnLcdsEvent",
+        "OnRegionLocaleChanged",
+        "OnServiceProxyAsyncEvent",
+        "OnServiceProxyMethodEvent",
+        "OnServiceProxyUuidEvent"
+    ];
 
     ws.on('open', () => {
+        localHelpEvents.forEach(event => {
+            ws.send(JSON.stringify([5, event]));
+            console.log("Subscribed to:", event)
+        });
+        /*
         Object.entries(helpData.events).forEach(([name, desc]) => {
             if(name === 'OnJsonApiEvent') return;
             ws.send(JSON.stringify([5, name]));
         });
+        */
         console.log('Connected to websocket!');
     });
 
